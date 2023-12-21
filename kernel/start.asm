6efac5587027519d169d41ccf89fde7715b5a726
Ry <ryanst755@gmail.com>
2023-12-21 15:24:07 -0800

    kernel/vfs + init: Integrate the disk ID into the filename

--- kernel/start.asm
+++ kernel/start.asm
@@ -2,16 +2,10 @@
 
 const SYSTEM_STACK: 0x01FFF800
 
-    call EntryCheck
-    ifz rjmp 0
-
     mov rsp, SYSTEM_STACK
+
+    ; the bootloader passed the boot disk id in r0
     call Main
 
     ; Main() should never return, but just in case it does, hang
     rjmp 0
-
-EntryCheck:
-    ; TODO: implement this
-    mov r0, 1
-    ret
